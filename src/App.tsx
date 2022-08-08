cc702f3a9c315c8bfb4888226004d91c46ab6f88
김영목 <mokyoungg@gmail.com>
2022-08-08 14:29:18 +0900

    feat: practice react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ImageList from 'ImageList/ImageList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
-  return <ImageList />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ImageList />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
